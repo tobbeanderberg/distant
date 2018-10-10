1f64fda72d180bd952e2bc17342a66d6483f12dd
Tobias Anderberg <tobias.anderberg@agical.se>
2018-10-10 13:08:54 +0200

    Can now move camera with WASD and arrows, but the movement is along rows and columns and not along actual view axis

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    float speed = 10;
+
 	void Start () {
 	}
 	
 	void Update () {
-
+        if(Input.GetKey(KeyCode.A) == true || Input.GetKey(KeyCode.LeftArrow)) {
+            transform.position += new Vector3 (-1 * Time.deltaTime * speed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.D) == true || Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += new Vector3(+1 * Time.deltaTime * speed, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.W) == true || Input.GetKey(KeyCode.UpArrow)) {
+            transform.position += new Vector3(0.0f, 0.0f, +1 * Time.deltaTime * speed);
+        }
+        if (Input.GetKey(KeyCode.S) == true || Input.GetKey(KeyCode.DownArrow)) {
+            transform.position += new Vector3(0.0f, 0.0f, -1 * Time.deltaTime * speed);
+        }
 	}
 }
