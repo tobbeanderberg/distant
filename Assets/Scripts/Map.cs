fa17393456ad85b0f934237b0e47be79c58c9238
Tobias Anderberg <tobias.anderberg@agical.se>
2018-10-10 10:32:48 +0200

    Setting initial position of camera and some refactoring

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,21 +5,34 @@ using UnityEngine;
 public class Map : MonoBehaviour {
 
     public GameObject hexPrefab;
-    //public GameObject camera;
+    public GameObject mainCamera;
 
     public int numTilesX;
     public int numTilesY;
 
     float offRowColumnOffset = 0.9f;
-    float rowOffset = 1.6f;
+    float rowHeight = 1.6f;
+    float tileWidth = 1.84f;
 
 	void Start () {
         for (int x = 0; x < numTilesX; x++) {
             for (int y = 0; y < numTilesY; y++) {
-                Instantiate(hexPrefab, new Vector3(x * 1.84f + (y%2 == 0 ? 0 : offRowColumnOffset), 0, ((y*rowOffset))), Quaternion.identity);
+                Instantiate(hexPrefab, 
+                            new Vector3(x * tileWidth + PossibleXOffset(y), 
+                                        0, 
+                                        (y * rowHeight)), 
+                            Quaternion.identity);
             }
         }
-        //camera.transform.position = new Vector3(0, 10f, -3f);
-        //camera.transform.rotation = Quaternion.Euler(45, 30, 0);
+        SetInitialCameraLocation();
 	}
+
+    private void SetInitialCameraLocation() {
+        mainCamera.transform.position = new Vector3(0, 10f, -3f);
+        mainCamera.transform.rotation = Quaternion.Euler(45, 30, 0);
+    }
+
+    private float PossibleXOffset(int row) {
+        return row % 2 == 0 ? 0 : offRowColumnOffset;
+    }
 }
