80d022d9da984ab96fee97b6b53e345071ed6bf6
Tobias Anderberg <tobias.anderberg@agical.se>
2018-10-10 10:12:16 +0200

    Map initiated on startup

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public GameObject hexPrefab;
+    //public GameObject camera;
+
+    public int numTilesX;
+    public int numTilesY;
+
+    float offRowColumnOffset = 0.9f;
+    float rowOffset = 1.6f;
+
+	void Start () {
+        for (int x = 0; x < numTilesX; x++) {
+            for (int y = 0; y < numTilesY; y++) {
+                Instantiate(hexPrefab, new Vector3(x * 1.84f + (y%2 == 0 ? 0 : offRowColumnOffset), 0, ((y*rowOffset))), Quaternion.identity);
+            }
+        }
+        //camera.transform.position = new Vector3(0, 10f, -3f);
+        //camera.transform.rotation = Quaternion.Euler(45, 30, 0);
+	}
+}
