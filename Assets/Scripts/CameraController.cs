d19ffe8c09aa457d9117b8ca0e376fa17d13a073
Tobias Anderberg <tobias.anderberg@agical.se>
2018-10-10 15:15:13 +0200

    Movement now in natural camera directions

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,16 +11,29 @@ public class CameraController : MonoBehaviour {
 	
 	void Update () {
         if(Input.GetKey(KeyCode.A) == true || Input.GetKey(KeyCode.LeftArrow)) {
-            transform.position += new Vector3 (-1 * Time.deltaTime * speed, 0.0f, 0.0f);
+            transform.position += -transform.right * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.D) == true || Input.GetKey(KeyCode.RightArrow)) {
-            transform.position += new Vector3(+1 * Time.deltaTime * speed, 0.0f, 0.0f);
+            transform.position += transform.right * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.W) == true || Input.GetKey(KeyCode.UpArrow)) {
-            transform.position += new Vector3(0.0f, 0.0f, +1 * Time.deltaTime * speed);
+            transform.position += ForwardDirection(transform) * Time.deltaTime * speed;
         }
         if (Input.GetKey(KeyCode.S) == true || Input.GetKey(KeyCode.DownArrow)) {
-            transform.position += new Vector3(0.0f, 0.0f, -1 * Time.deltaTime * speed);
+            transform.position += -ForwardDirection(transform) * Time.deltaTime * speed;
         }
 	}
+
+    private Vector3 ForwardDirection(Transform focus) {
+        // Get the natural forward transition
+        Vector3 naturalForward = focus.forward;
+        // Nullify its y-component
+        naturalForward.y = 0;
+        // Normalize xz-plane and align it with the chosen y-axis
+        Vector3 fixedForward = naturalForward.normalized;
+
+        // This gives us a vector whose xz-planar speed is normalized
+        // while preserving the fixed Y component
+        return fixedForward;
+    }
 }
